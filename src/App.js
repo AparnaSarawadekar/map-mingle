f818a7103a5f0a56654ccd092ba5735ca124a8f4
Aparna Sarawadekar <aparnavs7999@gmail.com>
2024-03-19 09:10:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Header";
+import Map from "./components/Map";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Map />
+    </>
+  );
+}
+
+export default App;
